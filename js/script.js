a24e0183b9c5fa7c9607df7acc1dc50a1df4fcc5
Kamron <bahtierhasanzoda4@gmail.com>
2023-05-14 15:58:08 +0500

    finish movies

--- js/script.js
+++ js/script.js
@@ -1,23 +1,228 @@
 import { movies } from "../modules/db.js";
 
-let cont = document.querySelector('.promo__interactive-list')
-let com = document.querySelector('.promo__genre')
-let tit = document.querySelector('.promo__title')
-let des = document.querySelector('.promo__descr')
-let im = document.querySelector('.im')
-let poisk = document.querySelector('.poisk')
+let ul = document.querySelector('.promo__interactive-list')
+let promo__bg = document.querySelector('.promo__bg')
+let promo__genre = document.querySelector(".promo__genre");
+let promo__title = document.querySelector(".promo__title");
+let promo__descr = document.querySelector(".promo__descr")
+let imdb = document.querySelector(".imdb");
+let reserch = document.querySelector(".reserch");
+let search = document.querySelector('#search')
+let genUl = document.querySelector('.genresUl')
+let genres_arr = ['All']
+let modal = document.querySelector('.modal')
 
-const movieDb = [];
+let massiv = movies
 
-let sorted = movies
-movieDb.push(sorted)
+search.onkeyup = () => {
+    let filtered = massiv.filter(item => {
+        let title = item.Title.toLowerCase()
+        let value = search.value.toLowerCase().trim()
 
-sorted.forEach(i => {
-    movieDb.push(i.Title)
-    movieDb.sort()
+        if (title.includes(value)) {
+            return item
+        }
+    })
+
+    reload(filtered)
+}
+
+function reload(arr) {
+    ul.innerHTML = ""
+    changeFilm(arr[0])
+
+    arr.forEach((movie, index) => {
+        genres_arr.push(movie.Genre)
+
+        let li = document.createElement('li')
+        let del = document.createElement('div')
+
+        li.innerHTML = `${index + 1}. ${movie.Title}`
+        li.classList.add('promo__interactive-item')
+        del.classList.add('delete')
+
+        li.onclick = () => {
+            modal.style.display = 'block'
+            changeFilm(movie)
+            createTable(movie)
+        }
+
+        del.onclick = () => {
+            li.remove()
+            massiv = massiv.filter(el => el.ID != index)
+            console.log(massiv);
+        }
+
+        li.append(del)
+        ul.append(li)
+    });
+}
+
+reload(massiv.sort((a, b) => a.Title > b.Title ? 1 : -1))
+
+function createTable(img) {
+    modal.innerHTML = ''
+    let h1 = document.createElement('h1')
+    let p = document.createElement('p')
+    let contt = document.createElement('div')
+    let close_img = document.createElement('img')
+    let right = document.createElement('div')
+    let table = document.createElement('table')
+    let tbody = document.createElement('tbody')
+    let tr1 = document.createElement('tr')
+    let th1_1 = document.createElement('th')
+    let th1_2 = document.createElement('th')
+    let tr2 = document.createElement('tr')
+    let th2_1 = document.createElement('th')
+    let th2_2 = document.createElement('th')
+    let tr3 = document.createElement('tr')
+    let th3_1 = document.createElement('th')
+    let th3_2 = document.createElement('th')
+    let tr4 = document.createElement('tr')
+    let th4_1 = document.createElement('th')
+    let th4_2 = document.createElement('th')
+    let tr5 = document.createElement('tr')
+    let th5_1 = document.createElement('th')
+    let th5_2 = document.createElement('th')
+    let tr6 = document.createElement('tr')
+    let th6_1 = document.createElement('th')
+    let th6_2 = document.createElement('th')
+    let tr7 = document.createElement('tr')
+    let th7_1 = document.createElement('th')
+    let th7_2 = document.createElement('th')
+    let tr8 = document.createElement('tr')
+    let th8_1 = document.createElement('th')
+    let th8_2 = document.createElement('th')
+    let tr9 = document.createElement('tr')
+    let th9_1 = document.createElement('th')
+    let th9_2 = document.createElement('th')
+    let tr10 = document.createElement('tr')
+    let th10_1 = document.createElement('th')
+    let th10_2 = document.createElement('th')
+    let tr11 = document.createElement('tr')
+    let th11_1 = document.createElement('th')
+    let th11_2 = document.createElement('th')
+
+    close_img.src = './img/close.svg'
+
+    h1.innerHTML = img.Title
+    p.innerHTML = img.Genre
+    th1_1.innerHTML = 'Рейтинги:'
+    th1_2.innerHTML = img.Ratings[0].Value
+    th2_1.innerHTML = 'Слоган:'
+    th2_2.innerHTML = img.Title
+    th3_1.innerHTML = 'Дата выхода:'
+    th3_2.innerHTML = img.Year
+    th4_1.innerHTML = 'Страна:'
+    th4_2.innerHTML = img.Language
+    th5_1.innerHTML = 'Режиссер:'
+    th5_2.innerHTML = img.Director
+    th6_1.innerHTML = 'Жанр:'
+    th6_2.innerHTML = img.Genre
+    th7_1.innerHTML = 'В качестве:'
+    th7_2.innerHTML = '1080p'
+    th8_1.innerHTML = 'В переводе:'
+    th8_2.innerHTML = 'Нету'
+    th9_1.innerHTML = 'Время:'
+    th9_2.innerHTML = img.Runtime
+    th10_1.innerHTML = 'Из серии:'
+    th10_2.innerHTML = img.Production
+    th11_1.innerHTML = 'В ролях актеры:'
+    th11_2.innerHTML = img.Actors
+
+    close_img.classList.add('cls')
+    right.classList.add('right')
+    contt.classList.add('cont')
+
+    modal.style.backgroundImage = `url(${img.Poster})`
+    modal.style.transition = '1s ease'
+
+    close_img.onclick = () => {
+        modal.style.display = 'none'
+    }
+
+    modal.append(close_img, h1, p, contt)
+    contt.append(right)
+    table.append(tbody)
+    tr1.append(th1_1, th1_2)
+    tr2.append(th2_1, th2_2)
+    tr3.append(th3_1, th3_2)
+    tr4.append(th4_1, th4_2)
+    tr5.append(th5_1, th5_2)
+    tr6.append(th6_1, th6_2)
+    tr7.append(th7_1, th7_2)
+    tr8.append(th8_1, th8_2)
+    tr9.append(th9_1, th9_2)
+    tr10.append(th10_1, th10_2)
+    tr11.append(th11_1, th11_2)
+    tbody.append(tr1, tr2, tr3, tr4, tr5, tr6, tr7, tr8, tr9, tr10, tr11)
+    right.append(table)
+}
+
+let btns = document.querySelectorAll(".promo__menu-item")
+
+function changeFilm(props) {
+    promo__bg.style.backgroundImage = `url("${props.Poster}")`
+    promo__bg.style.transition = '1s ease'
+    promo__genre.innerHTML = props.Genre
+    promo__title.innerHTML = props.Title
+    promo__descr.innerHTML = props.Plot
+    imdb.innerHTML = `IMDb: ${props.imdbRating}`
+    reserch.innerHTML = `Кинопоиск: ${props.Metascore}`
+}
+
+btns.forEach(btn => {
+    btn.onclick = () => {
+        btns.forEach(btn => {
+            btn.classList.remove('promo__menu-item_active')
+        })
+        btn.classList.add('promo__menu-item_active')
+    }
+
 })
 
-// 1
+genres_arr = [...new Set(genres_arr)]
+
+generateGenres(genres_arr)
+function generateGenres(arr) {
+    genUl.innerHTML = ""
+
+    for (let item of arr) {
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+
+        if (arr.indexOf(item) === 0) {
+            a.classList.add('promo__menu-item_active')
+        }
+
+        a.classList.add('promo__menu-item')
+        a.href = "#"
+        a.innerHTML = item
+
+        li.append(a)
+        genUl.append(li)
+
+        li.onclick = () => {
+            genUl.childNodes.forEach(elem => elem.firstChild.classList.remove('promo__menu-item_active'))
+
+            li.firstChild.classList.add('promo__menu-item_active')
+
+            let filtered = massiv.filter(elem => {
+                let genre = elem.Genre.toLowerCase()
+                if (item.toLowerCase() === genre) {
+                    return elem
+                } else if (item.toLowerCase() === 'all') {
+                    reload(massiv.sort((a, b) => a.Title > b.Title ? 1 : -1))
+                }
+            })
+
+
+            if (filtered.length > 0) reload(filtered)
+        }
+
+    }
+}
+
 let adds = document.querySelectorAll('.promo__adv img')
 let add_text = document.querySelector('.promo__adv-title')
 
@@ -25,51 +230,3 @@ adds.forEach((img) => {
     img.remove()
     add_text.remove()
 })
-
-// 2
-let genre = document.querySelectorAll('.promo__menu-item')
-
-genre.forEach((type) => {
-    type.onclick = () => {
-        genre.forEach(el => el.classList.remove('promo__menu-item_active'))
-        type.classList.add('promo__menu-item_active')
-    }
-})
-
-// 3
-let back = document.querySelector('.promo__bg')
-back.style.backgroundImage = 'url(./img/bg.jpg)'
-
-// create
-function reload(arr, place) {
-    place.innerHTML = ''
-    for (let item of arr) {
-        let li = document.createElement('li')
-        let del = document.createElement('div')
-
-        li.innerHTML = (arr.indexOf(item) + 1) + '. ' + item.Title
-        li.classList.add('promo__interactive-item')
-        del.classList.add('delete')
-
-        li.onclick = () => {
-            back.style.background = `url("${item.Poster}")`
-            com.innerHTML = item.Genre
-            des.innerHTML = item.Writer.slice(0, 120)
-            im.innerHTML = `IMDb: ${item.imdbRating}`
-            poisk.innerHTML = `Кинопоиск: ${Math.round(item.imdbRating)}`
-        }
-
-        del.onclick = () => {
-            del.parentElement.remove()
-                if(item.Title != arr.forEach(i => i.Title)) {
-                    sorted = sorted.filter(el => el.Title != item.Title)
-                    console.log(sorted);
-                }
-        }
-
-        li.append(del)
-        place.append(li)
-    }
-}
-
-reload(sorted, cont)
--- modules/db.js
+++ modules/db.js
@@ -16,17 +16,17 @@ let movies = [
         "Awards": "Nominated for 2 Oscars. Another 52 wins & 100 nominations.",
         "Poster": "https://3.bp.blogspot.com/-UTaikqU2-Eg/WNUSKUnZ4CI/AAAAAAAAiBI/t9joQ-25CGYCUlk8PKoNf3RyCT0weJJJACLcB/s1600/GOTGV2.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "8.0/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "92%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "76/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "8.0/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "92%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "76/100"
+        }
         ],
         "Metascore": "76",
         "imdbRating": "8.0",
@@ -56,17 +56,17 @@ let movies = [
         "Awards": "Nominated for 1 Oscar. Another 15 wins & 57 nominations.",
         "Poster": "https://v1.popcornnews.ru/k2/news/1200/upload/3mKYbr.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.6/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "85%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "67/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "7.6/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "85%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "67/100"
+        }
         ],
         "Metascore": "67",
         "imdbRating": "7.6",
@@ -96,17 +96,17 @@ let movies = [
         "Awards": "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
         "Poster": "https://cdnn21.img.ria.ru/images/155277/80/1552778074_0:0:1108:623_600x0_80_0_0_0d3812d6d4a03d9f659db043ab84af16.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "8.0/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "91%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "69/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "8.0/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "91%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "69/100"
+        }
         ],
         "Metascore": "69",
         "imdbRating": "8.0",
@@ -136,17 +136,17 @@ let movies = [
         "Awards": "8 wins & 49 nominations.",
         "Poster": "https://www.comicbookraw.com/wp-content/uploads/2020/03/age-ultron-avengers-of-.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.3/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "75%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "66/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "7.3/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "75%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "66/100"
+        }
         ],
         "Metascore": "66",
         "imdbRating": "7.3",
@@ -176,17 +176,17 @@ let movies = [
         "Awards": "Nominated for 1 Oscar. Another 46 wins & 73 nominations.",
         "Poster": "https://lifehacker.ru/wp-content/uploads/2019/04/InfinityWar5aabd55fed5fa.0_1556501450.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "8.4/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "85%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "68/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "8.4/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "85%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "68/100"
+        }
         ],
         "Metascore": "68",
         "imdbRating": "8.4",
@@ -216,17 +216,17 @@ let movies = [
         "Awards": "Nominated for 1 Oscar. Another 69 wins & 102 nominations.",
         "Poster": "https://storage.kun.uz/source/4/tqosJUrhyALjXweP-PVSBz5uWJ78KOyq.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "8.4/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "94%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "78/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "8.4/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "94%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "78/100"
+        }
         ],
         "Metascore": "78",
         "imdbRating": "8.4",
@@ -256,17 +256,17 @@ let movies = [
         "Awards": "Nominated for 1 Oscar. Another 19 wins & 66 nominations.",
         "Poster": "https://img08.rl0.ru/afisha/e1200x600i/daily.afisha.ru/uploads/images/7/71/771e25c072bc45afb06a4581fd5f92d7.jpeg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.5/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "89%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "72/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "7.5/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "89%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "72/100"
+        }
         ],
         "Metascore": "72",
         "imdbRating": "7.5",
@@ -296,17 +296,17 @@ let movies = [
         "Awards": "7 wins & 10 nominations.",
         "Poster": "https://www.kino-teatr.ru/art/4779/65726.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.4/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "92%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "73/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "7.4/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "92%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "73/100"
+        }
         ],
         "Metascore": "73",
         "imdbRating": "7.4",
@@ -336,17 +336,17 @@ let movies = [
         "Awards": "10 wins & 22 nominations.",
         "Poster": "https://kinoreporter.ru/wp-content/uploads/2019/07/00-31.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.5/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "91%"
-            },
-            {
-                "Source": "Metacritic",
-                "Value": "69/100"
-            }
+            "Source": "Internet Movie Database",
+            "Value": "7.5/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "91%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "69/100"
+        }
         ],
         "Metascore": "69",
         "imdbRating": "7.5",
@@ -376,18 +376,18 @@ let movies = [
         "Awards": "Nominated for 1 BAFTA Film Award. Another 4 wins & 33 nominations.",
         "Poster": "https://www.film.ru/sites/default/files/styles/thumb_1024x450/public/trailers_frame/ant-man-2015-3_1.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.3/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "82%"
-            },
-            {
-                "Source": "Metacritic",
+            "Source": "Internet Movie Database",
+            "Value": "7.3/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "82%"
+        },
+        {
+            "Source": "Metacritic",
 
-                "Value": "64/100"
-            }
+            "Value": "64/100"
+        }
         ],
         "Metascore": "64",
         "imdbRating": "7.3",
@@ -417,18 +417,18 @@ let movies = [
         "Awards": "Nominated for 1 BAFTA Film Award. Another 4 wins & 33 nominations.",
         "Poster": "https://www.film.ru/sites/default/files/styles/thumb_1024x450/public/trailers_frame/ant-man-2015-3_1.jpg",
         "Ratings": [{
-                "Source": "Internet Movie Database",
-                "Value": "7.3/10"
-            },
-            {
-                "Source": "Rotten Tomatoes",
-                "Value": "82%"
-            },
-            {
-                "Source": "Metacritic",
+            "Source": "Internet Movie Database",
+            "Value": "7.3/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "82%"
+        },
+        {
+            "Source": "Metacritic",
 
-                "Value": "64/100"
-            }
+            "Value": "64/100"
+        }
         ],
         "Metascore": "64",
         "imdbRating": "7.3",
@@ -443,4 +443,5 @@ let movies = [
     }
 ]
 
-export {movies}
+
+export { movies }
